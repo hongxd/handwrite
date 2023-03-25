7e7751a8200703b688122aaa0b70ab8cc3f99b8b
ddd <d4980@qq.com>
2023-03-25 16:13:12 +0800

    feat: type

---
+++ type.mjs
@@ -0,0 +1,12 @@
+function getType(arg) {
+  const type = typeof arg;
+  if(type === "number" && isNaN(arg)) return "NaN";
+  if (type !== "object") return type;
+  if (arg?.constructor) {
+    return arg.constructor.name;
+  }
+  return Object.prototype.toString.call(arg).slice(8,-1).toLowerCase();
+}
+class A{}
+console.log(getType(null))
+console.log(getType(new A))
