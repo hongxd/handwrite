7a78aae8707c298e479d4c0dd157db5d0831ab7b
ddd <d4980@qq.com>
2023-03-25 16:17:06 +0800

    feat: 柯里化

--- curring.mjs
+++ curring.mjs
@@ -1,24 +1,21 @@
-function add() {
-  // 将传入的不定参数转为数组对象
-  let args = Array.prototype.slice.call(arguments);
-
-  // 递归：内部函数里面进行自己调用自己
-  // 当 add 函数不断调用时，把第 N+1 个括号的参数加入到第 N 个括号的参数里面
-  let inner = function() {
-    args.push(...arguments);
-    return inner;
-  }
-  
-  inner.toString = function() {
-    // args 里的值不断累加
-    return args.reduce(function(prev, cur) {
-      return prev + cur;  
-    });
+function curry(func) {
+  return function curried(...args) {
+    if (args.length >= func.length) {
+      return func.apply(this, args);
+    } else {
+      return function(...args2) {
+        return curried.apply(this, args.concat(args2));
+      }
+    }
   };
 
-  return inner;
+}
+function sum(a, b, c) {
+  return a + b + c;
 }
 
-// 测试一下
-let result = add(1)(2)(3)(4);
-console.log(result.toString());
+let curriedSum = curry(sum);
+
+console.log( curriedSum(1, 2, 3) ); // 6，仍然可以被正常调用
+console.log( curriedSum(1)(2,3) ); // 6，对第一个参数的柯里化
+console.log( curriedSum(1)(2)(3) ); // 6，全柯里化
