1d0bea15adb9856c512b11c304465a34fdd64f3f
ddd <d4980@qq.com>
2023-03-25 16:16:43 +0800

    feat: random

---
+++ random.mjs
@@ -0,0 +1,13 @@
+function random(arr) {
+  const len = arr.length;
+  for (let i = len - 1; i > 0; i--) {
+    const rd = Math.floor(Math.random() * (i - 1));
+    const temp = arr[i];
+    arr[i] = arr[rd];
+    arr[rd] = temp;
+  }
+}
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log(arr)
+random(arr)
+console.log(arr)
