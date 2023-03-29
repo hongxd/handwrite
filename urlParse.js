ff914166bf6c214ce910e185af3d70ce405047f7
ddd <d4980@qq.com>
2023-03-29 21:47:07 +0800

    feat: urlParse

---
+++ urlParse.js
@@ -0,0 +1,21 @@
+// 解析URL，实现一个函数parseUrl使，
+// parseUrl('https://www.meituan.com/index.html?a=test&b=2.1')
+// 返回 {a:'test', b: 2.1}
+
+/**
+ *
+ * @param {string} url
+ * @returns
+ */
+function parseUrl(url) {
+  const index = url.indexOf("?");
+  const strs = url.slice(index + 1).split("&");
+  const obj = {};
+  for (let i = 0; i < strs.length; i++) {
+    const [key,value] = strs[i].split("=");
+    obj[key] = value
+  }
+  return obj;
+}
+let str = "https://www.meituan.com/index.html?a=test&b=666v";
+console.log("res ", parseUrl(str));
