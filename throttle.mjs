3084e3c40b829f5bf0e90f6fffb063cfdcf2b631
ddd <d4980@qq.com>
2023-04-07 22:22:01 +0800

    fix: 修正节流函数

--- throttle.mjs
+++ throttle.mjs
@@ -1,19 +1,12 @@
 function throttle(func, wait, options = {}) {
-  let timeout, context, args, result;
+  let timeout, args;
   let previous = 0;
-
-  const later = function() {
-    previous = options.leading === false ? 0 : Date.now();
-    timeout = null;
-    result = func.apply(context, args);
-    if (!timeout) context = args = null;
-  };
+  const { leading, trailing } = options;
 
   const throttled = function() {
     const _now = Date.now();
-    if (!previous && options.leading === false) previous = _now;
+    if (!previous && leading === false) previous = _now;
     let remaining = wait - (_now - previous);
-    context = this;
     args = arguments;
     if (remaining <= 0 || remaining > wait) {
       if (timeout) {
@@ -21,21 +14,23 @@ function throttle(func, wait, options = {}) {
         timeout = null;
       }
       previous = _now;
-      result = func.apply(context, args);
-      if (!timeout) context = args = null;
-    } else if (!timeout && options.trailing !== false) {
-      timeout = setTimeout(later, remaining);
+      func.apply(this, args);
+    } else if (!timeout && trailing !== false) {
+      timeout = setTimeout(() => {
+        previous = leading === false ? 0 : Date.now();
+        func.apply(this, args);
+        timeout = null
+      }, remaining);
     }
-    return result;
   };
 
   throttled.cancel = function() {
     clearTimeout(timeout);
     previous = 0;
-    timeout = context = args = null;
+    timeout = args = null;
   };
 
   return throttled;
 }
-const fn = throttle((i) => console.log(i),100,{trailing:true});
-for(let i = 0; i < 2**20; i++) fn(i)
+const fn = throttle((i) => console.log(i),200,{trailing:true});
+for(let i = 0; i < 2**22; i++) fn(i)
