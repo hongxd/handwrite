37420d9811ebf20573a4844f67e8e1fc288dfc93
ddd <d4980@qq.com>
2023-04-07 15:40:30 +0800

    feat: 节流函数

--- throttle.mjs
+++ throttle.mjs
@@ -1,49 +1,41 @@
-function throttle(fn, interval = 0, { leading = true, trailing = false } = {}) {
-  if (typeof fn !== "function") {
-    throw new TypeError("fn must be a function");
-  }
-  // 双指针法
-  return function FN(...args) {
-    let startTime = 0;
-    let timer = null;
-    return new Promise((resolve) => {
-      const currentTime = Date.now();
-      const waitTime = interval - (currentTime - startTime);
+function throttle(func, wait, options = {}) {
+  let timeout, context, args, result;
+  let previous = 0;
 
-      if (!leading && startTime === 0) {
-        startTime = currentTime;
-      }
-      if (waitTime <= 0) {
-        resolve(fn.apply(this, args));
-        if (timer) clearTimeout(timer);
-        timer = null;
-        startTime = currentTime;
-        return;
-      }
-      if (trailing && !timer) {
-        clearTimeout(timer);
-        timer = setTimeout(() => {
-          resolve(fn.apply(this, args));
-          startTime = Date.now();
-          timer = null;
-        }, waitTime);
-      }
-      FN.cancel = () => {
-        clearTimeout(timer);
-        timer = null;
-        startTime = 0;
-      };
-    });
+  const later = function() {
+    previous = options.leading === false ? 0 : Date.now();
+    timeout = null;
+    result = func.apply(context, args);
+    if (!timeout) context = args = null;
   };
+
+  const throttled = function() {
+    const _now = Date.now();
+    if (!previous && options.leading === false) previous = _now;
+    let remaining = wait - (_now - previous);
+    context = this;
+    args = arguments;
+    if (remaining <= 0 || remaining > wait) {
+      if (timeout) {
+        clearTimeout(timeout);
+        timeout = null;
+      }
+      previous = _now;
+      result = func.apply(context, args);
+      if (!timeout) context = args = null;
+    } else if (!timeout && options.trailing !== false) {
+      timeout = setTimeout(later, remaining);
+    }
+    return result;
+  };
+
+  throttled.cancel = function() {
+    clearTimeout(timeout);
+    previous = 0;
+    timeout = context = args = null;
+  };
+
+  return throttled;
 }
-
-let i = 0;
-const myThrottle = throttle(() => {
-  i++;
-  console.log("111");
-}, 300);
-
-setInterval(() => {
-  myThrottle();
-  console.log(i);
-}, 100);
+const fn = throttle((i) => console.log(i),100,{trailing:true});
+for(let i = 0; i < 2**20; i++) fn(i)
