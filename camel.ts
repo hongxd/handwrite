5094496f06f53eccf37c425aaca5132288666a2a
ddd <d4980@qq.com>
2023-04-09 16:12:06 +0800

    feat: 字符串转驼峰

---
+++ camel.ts
@@ -0,0 +1,9 @@
+function toCamelCase(str) {
+  let arr = str.split(/[-_]/);
+  for (let i = 1; i < arr.length; i++) {
+    arr[i] = arr[i].charAt(0).toUpperCase() + arr[i].slice(1);
+  }
+  return arr.join("");
+}
+
+console.log(toCamelCase('hello-world')); // helloWorld console.log(toCamelCase('hello_world')); // helloWorld console.log(toCamelCase('hello-world-goodbye')); // helloWorldGoodbye
