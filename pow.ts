96aa57c3012b6380acf032a837adccfc64ead06c
ddd <d4980@qq.com>
2023-04-11 22:07:05 +0800

    feat: pow

---
+++ pow.ts
@@ -0,0 +1,7 @@
+function pow(x, n) {
+  if (n === 0) return 1;
+  if (n < 0) return 1 / pow(x, -n);
+  if (n % 2 !== 0) return x * pow(x, n - 1);
+  return pow(x * x, n / 2);
+}
+console.log(pow(2, 6));
