178b7c613e0bc9256cbc836b736a053b849e8f91
ddd <d4980@qq.com>
2023-04-11 23:25:13 +0800

    refactor: 重写Promise.all

--- promise.all.mjs
+++ promise.all.mjs
@@ -1,30 +1,32 @@
-/**
- *
- * @param {Iterable<T | PromiseLike<T>>} value
- */
-Promise.myAll = async function (value) {
-  const res = [];
-  for (let i = 0; i < value.length; i++) {
-    const promise = value[i];
-    try {
-      res.push(await promise);
-    } catch {
-      return Promise.reject(promise);
+Promise.myAll = function (promiseArray) {
+  return new Promise((resolve, reject) => {
+    const len = promiseArray.length;
+    const res = new Array(len);
+    let counter = 0;
+    for (let i = 0; i < len; i++) {
+      const p = Promise.resolve(promiseArray[i]); // 确保是一个promise
+      p.then((val) => {
+        counter++;
+        res[i] = val;
+        if (counter === len) resolve(res);
+      }, reject);
     }
-  }
-  return res;
+  });
 };
 const p = [
   Promise.resolve(1),
-  Promise.reject(2),
+  Promise.resolve(2),
   Promise.resolve(3),
   Promise.resolve(4),
   Promise.resolve(5),
 ];
 Promise.myAll(p)
-  .then((reason) => {
-    console.log(reason);
-  }, (reason) => {
-    console.log(reason);
-  })
+  .then(
+    (reason) => {
+      console.log(reason);
+    },
+    (reason) => {
+      console.log(reason);
+    }
+  )
   .catch((e) => console.log(e));
